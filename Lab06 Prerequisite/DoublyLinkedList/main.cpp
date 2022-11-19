6303a7b0bb31e4839cca53ae41ce25262ecdb2f1
Mehedi Hasan Bijoy <bijoyx3@gmail.com>
2022-11-19 20:41:55 +0600

    LinkedLists/Basics

---
+++ Lab06 Prerequisite/DoublyLinkedList/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node *prev;
+        Node *next;
+};
+
+Node *head = NULL;
+
+void insertAtHead(int value){
+    // ToDo
+}
+
+
+void deleteFromHead(){
+    // ToDo
+}
+
+
+void print(){
+    // ToDo
+}
+
+
+int main()
+{
+    // ToDo
+
+    return 0;
+}
